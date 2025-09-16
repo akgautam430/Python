9f3f751aa88adfef64b6e127fbbbdb55507406da
akgautam430 <akgautam430@gmail.com>
2025-09-16 16:28:35 +0530

    Initial commit

---
+++ Assignment 1.py
@@ -0,0 +1,16 @@
+# Task 1
+
+a = int(input('Enter the first number: '))
+b = int(input('Enter the second number: '))
+
+print('Addition: ', a + b)
+print('Subtraction: ', a - b)
+print('Multiplication: ', a * b)
+print('Division: ', a / b)
+
+# Task 2
+
+a = input('Enter first name: ')
+b = input('Enter second name: ')
+
+print('Hello, ' + a +' '+ b + '! Welcome to the Python Program.')
